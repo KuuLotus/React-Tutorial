29fd7caf29334d40fd9017b4fd6c9aeaefc021de
KuuLotus <kuulotusy@gmail.com>
2022-07-22 22:41:53 +0900

    [add]TodoList追加

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import "./App.css";
+import { useState, useRef } from "react";
+import TodoList from "./TodoList";
 
 function App() {
-  return <div>
-    <TodoList />
-  </div>;
+  const [todos, setTodos] = useState([
+    { id: 1, name: "Todo1", completed: false },
+  ]);
+
+  const todoNameRef = useRef();
+
+  const handleAddTodo = () => {
+    // タスクを追加する。
+    console.log(todoNameRef);
+  };
+
+  return (
+    <>
+      <TodoList todos={todos} />
+      <input type="text" ref={todoNameRef} />
+      <button onClick={handleAddTodo}>タスクを追加</button>
+      <button>完了したタスクの削除</button>
+      <div>残りのタスク:0</div>
+    </>
+  );
 }
 
 export default App;

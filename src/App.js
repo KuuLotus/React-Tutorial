ce79d6bd78fc62cd53b698fd878b177346cd8381
KuuLotus <kuulotusy@gmail.com>
2022-07-23 14:18:12 +0900

    [finisf]TodoList

--- src/App.js
+++ src/App.js
@@ -3,24 +3,39 @@ import { useState, useRef } from "react";
 import TodoList from "./TodoList";
 
 function App() {
-  const [todos, setTodos] = useState([
-    { id: 1, name: "Todo1", completed: false },
-  ]);
+  const [todos, setTodos] = useState([]);
 
   const todoNameRef = useRef();
 
   const handleAddTodo = () => {
     // タスクを追加する。
-    console.log(todoNameRef);
+    const name = todoNameRef.current.value;
+    if (name === "") return;
+    setTodos((prevTodos) => {
+      return [...prevTodos, { id: "1", name: name, completed: false }];
+    });
+    todoNameRef.current.value = null;
+  };
+
+  const toggleTodo = (name) => {
+    const newTodos = [...todos];
+    const todo = newTodos.find((todo) => todo.name === name);
+    todo.completed = !todo.completed;
+    setTodos(newTodos);
+  };
+
+  const handleClear = () => {
+    const newTodos = todos.filter((todo) => !todo.completed);
+    setTodos(newTodos);
   };
 
   return (
     <>
-      <TodoList todos={todos} />
+      <TodoList todos={todos} toggleTodo={toggleTodo} />
       <input type="text" ref={todoNameRef} />
       <button onClick={handleAddTodo}>タスクを追加</button>
-      <button>完了したタスクの削除</button>
-      <div>残りのタスク:0</div>
+      <button onClick={handleClear}>完了したタスクの削除</button>
+      <div>残りのタスク:{todos.filter((todo) => !todo.completed).length}</div>
     </>
   );
 }
